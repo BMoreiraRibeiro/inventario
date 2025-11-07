279ebc9a1dc3e3ec97d7d46777250e437ac7cc08
Bruno <bmoreiraribeiro@gmail.com>
2025-11-07 00:39:03 +0000

    PWA: add manifest and icon support for Android home screen installation

--- config.example.js
+++
@@ -1,6 +0,0 @@
-// Configuração da aplicação
-const CONFIG = {
-    // ALTERE A PASSWORD AQUI
-    // Copie este ficheiro para config.js e defina a sua password
-    PASSWORD: 'SUA_PASSWORD_AQUI'
-};
