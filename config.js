ec1ec91809ad873acba995d8e203568b709d783d
Bruno Miguel Moreira Ribeiro <52053428+BMoreiraRibeiro@users.noreply.github.com>
2026-01-31 04:36:14 +0000

    Set GITHUB_TOKEN in config.js
    Updated GITHUB_TOKEN with a new token value.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Configuração da aplicação
 const CONFIG = {
     PASSWORD: 'invent123',
-    GITHUB_TOKEN: '',  // Adicione seu token aqui após deploy
+    GITHUB_TOKEN: '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',
     GIST_ID: ''  // Deixar vazio, app cria automaticamente no primeiro sync
 };
