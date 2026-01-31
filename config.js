23a6b6cd584abcb5d9c7b2d85c18f0df3194da27
Bruno Miguel Moreira Ribeiro <52053428+BMoreiraRibeiro@users.noreply.github.com>
2026-01-31 05:08:42 +0000

    Update GIST_ID in config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 const CONFIG = {
     PASSWORD: 'invent123',
     GITHUB_TOKEN: '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',
-    GIST_ID: ''  // Deixar vazio, app cria automaticamente no primeiro sync
+    GIST_ID: 'f0135daf6d12a66272428c98d9b2048c'  // Deixar vazio, app cria automaticamente no primeiro sync
 };
